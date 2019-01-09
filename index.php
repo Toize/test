0e00e3328af881c00691d9eee2f57fd886cbfc02
Toize <djshuffster@gmail.com>
2019-01-08 20:09:09 -0500

    html

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
+$names = ['evan','austin','ollivia','chloe'];
+$math = money_format('%i', 23 * 23);
+?>
 
-echo 'it works';
+<html>
+<head>
+
+<script type="text/javascript">
+alert('boom');
+</script>
+</head>
+<body style="background-color:red; color:white;">
+<h1>Hello</h1>
+<p>lkfalkfjdjfkdjkf</p>
+<ul>
+<?php
+foreach($names As $name) {
+
+    echo "<li>$name</li>";
+}
+
+echo $math;
+?>
+</ul>
+</body>
+
+</html>
